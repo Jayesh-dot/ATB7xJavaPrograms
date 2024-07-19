09012ee88831e100db02fceccb33a9c9a02527e0
Jayesh <jayesh.venugopalan@gmail.com>
2024-07-19 18:14:40 +1200

    Sample Program from Lab071 to Lab077

---
+++ src/ex_14072024/Lab073.java
@@ -0,0 +1,12 @@
+package ex_14072024;
+
+public class Lab073 {
+    public static void main(String[] args) {
+        /* Program for concat() */
+        String str1 = "Hello";
+        String str2 = " Guys";
+        String str4 = str1.concat(str2); // Hello Guys
+        System.out.println("The String Value is : " + str4);
+        // SCAP -  4
+    }
+}
---
+++ src/ex_14072024/Lab077.java
@@ -0,0 +1,8 @@
+package ex_14072024;
+
+public class Lab077 {
+    public static void main(String[] args) {
+        // Non Primitive Types - String, Arrays, Class, ENUM
+        String s1 = "pramod";
+    }
+}
