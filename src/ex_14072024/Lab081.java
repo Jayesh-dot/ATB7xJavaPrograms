aa0d9c021f449239830412c6d126c4e284b11206
Jayesh <jayesh.venugopalan@gmail.com>
2024-07-19 18:22:34 +1200

    Sample Program from Lab078 to Lab081

---
+++ src/ex_14072024/Lab081.java
@@ -0,0 +1,15 @@
+package ex_14072024;
+
+import java.lang.Math;
+
+public class Lab081 {
+    public static void main(String[] args) {
+        //Program using math funtions
+
+        double sq = Math.sqrt(2);
+        int result = Math.max(10, 23);
+
+        System.out.println("Square root " + sq);
+        System.out.println("Result "+result);
+    }
+}
